ac8cd1983050fd729580035f1d97485319180816
jxi24 <joshua.p.isaacson@gmail.com>
2015-12-09 10:00:36 -0500

    Add getters for Data class

--- src/data/data.h
+++ src/data/data.h
@@ -11,6 +11,19 @@
 class Data {
     public:
         Data(std::string dataFile);
+        double GetXData(int i){return xData.at(i);};
+        // GetYData returns the y prediction for a given x value
+        double GetYData(double x);
+        // Overloaded function to return the ith entry in the data
+        double GetYData(int i){return yData.at(i);};
+        // The following two getters returns the upper and lower errors respectively. The
+        // error that is returned is given by: sqrt(Sys^2+Stat^2)
+        double GetYUpError(double x);
+        double GetYDnError(double x);
+        // Overloaded functions for the ith error entry
+        double GetYUpError(int i){return sqrt(yErrSysUp.at(i)*yErrSysUp.at(i)+yErrStatUp.at(i)+yErrStatUp.at(i));};
+        double GetYDnError(int i){return sqrt(yErrSysDn.at(i)*yErrSysDn.at(i)+yErrStatDn.at(i)+yErrStatDn.at(i));};
+
     protected:
 
     private:
